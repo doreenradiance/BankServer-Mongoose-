8a2e174f7f92b7051298e3f36ad609cb2775be25
Doreen Mensah <doreenradiance@gmail.com>
2021-03-06 13:08:31 +0000

    Formatted the code well

--- accountControllers.js
+++ accountControllers.js
@@ -6,10 +6,10 @@ const listAccountController = (req, res) => {
 
     if (id) {
         AccountModel.find({ _id: id })
-        .populate("bankId", "name location branch")
-        .then(accounts => {
-            res.json({ data: accounts })
-        }).catch(err => console.log(err));
+            .populate("bankId", "name location branch")
+            .then(accounts => {
+                res.json({ data: accounts })
+            }).catch(err => console.log(err));
     } else {
         AccountModel.find().then(accounts => {
             res.json({ data: accounts })
@@ -24,43 +24,44 @@ const createAccountController = (req, res) => {
 
     account.save().then(result => {
         if (result)
-        res.json({ message: "Create account successful.", data: result });
-        else(result)
+            res.json({ message: "Create account successful.", data: result });
+        else (result)
         res.json({ message: "Failed to create account." });
     }).catch(error => console.log(error));
 }
 const updateAccountController = (req, res) => {
     // update a bank
-    const { accountName, accountNumber,accountType,bank,id } = req.body;
+    const { accountName, accountNumber, accountType, bank, id } = req.body;
 
     AccountModel.findById(id).then(account => {
-        if (account) { 
+        if (account) {
             account.accountName = accountName;
             account.accountNumber = accountNumber;
             account.accountType = accountType;
             account.bank = bank
             account.save();
 
-            res.json({ message: "create account successful", data: account});
+            res.json({ message: "create account successful", data: account });
         }
 
-        res.json({ message: "Account cannot be found"});
+        res.json({ message: "Account cannot be found" });
     }).catch(err => console.log(err));
 
     // const updatedAccount = AcountModel.update({ accountName, accountNumber,accountType,bank });
     // res.json({ message: "create account successful", data: updatedAccount });
-// }
-// // const deleteAccountController = (req, res) => {
-// //     // delete an account
-// //     const { name } = req.body;
-// //     const deletedAccount = AccountModel.delete({ name });
-// //     res.json({ message: "deleted account", data: deletedAccount });
-// // }
+    // }
+    const deleteAccountController = (req, res) => {
+        // delete an account
+        const { name } = req.body;
+        const deletedAccount = AccountModel.delete({ name });
+        res.json({ message: "deleted account", data: deletedAccount });
+    }
 
 
-module.exports = {
-    listAccountController,
-    createAccountController,
-    updateAccountController,
-    // deleteAccount Controller
+    module.exports = {
+        listAccountController,
+        createAccountController,
+        updateAccountController,
+        deleteAccountController,
+    }
 }
