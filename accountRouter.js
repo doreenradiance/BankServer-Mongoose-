ebc7a955d784bb9e2c215f43e7399db39f322b6e
Doreen Mensah <doreenradiance@gmail.com>
2021-03-10 14:59:48 +0000

    Validated the post account

--- accountRouter.js
+++ accountRouter.js
@@ -1,15 +1,24 @@
 const router = require("express").Router();
+const {body,validationResult} = require("express-validator");
+
 const {listAccountController,
     createAccountController,
     updateAccountController,
-    deleteAccountController} = require("./controllers")
+    deleteAccountController} = require("./accountControllers")
 //routes
 // view account - get method
-router.get("/accounts/:id?", listAccountController)
+// router.get("/accounts/:id?",listAccountController)
+
 // // create account - post method
-router.post("/account", createAccountController)
+router.post("/account", 
+[body("accountName").notEmpty().withMessage("Account Name required"), 
+body("accountNumber").isNumeric().not().isEmpty().withMessage("Account Number required"),
+body("accountType").not().isEmpty().withMessage("Account Type required"),
+body("bankId").not().isEmpty().withMessage("bankId required")],
+createAccountController)
+
 // update account - put / patch method
-router.put("/account", updateAccountController)
+router.put("/account",updateAccountController)
 // // delete account - delete method
 // router.delete("/account", deleteAccountController)
 
