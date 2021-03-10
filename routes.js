0b5f8818119a47fb290f54ff688feb4c275634d9
Doreen Mensah <doreenradiance@gmail.com>
2021-03-10 15:05:18 +0000

    Formatted the code

--- routes.js
+++ routes.js
@@ -11,11 +11,7 @@ router.post("/bank", createBankController)
 // update bank - put / patch method
 router.put("/bank", updateBankController)
 // // delete bank - delete method
-// router.delete("/bank", deleteBankController)
+router.delete("/bank", deleteBankController)
 
-// connect database and start server
-// mongoose.connect("mongodb+srv://codetrainUser:doreen1@cluster0.dlih7.mongodb.net/codetrain?retryWrites=true&w=majority",
-// {useNewUrlParser: true, userUnified:  true, useUnifiedTopology: true}
-// )
 
 module.exports = router;
