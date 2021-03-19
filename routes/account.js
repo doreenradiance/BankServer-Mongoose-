452489cae07171f9cabe8e7f828396174da990f1
Doreen Mensah <doreenradiance@gmail.com>
2021-03-19 11:51:45 +0000

    Created the account post method.

--- routes/account.js
+++ routes/account.js
@@ -1,6 +1,18 @@
+const express = require('express');
 const { createAccountController, listAccountController } = require("../controllers/account");
+const router = express.Router();
 
+    // // create account - post method
+    router.post("/account", 
+    [body("accountName").notEmpty().withMessage("Account Name required"), 
+    body("accountType").notEmpty().withMessage("Account Location required"),
+    body("accountNumber").not().isEmpty().withMessage("Account Number required"),
+    body("bankId").not().isEmpty().withMessage("bankId required")],
+    createAccountController)
+    
+    
 
-server.post('/account', createAccountController);
+router.post('/account', createAccountController);
+router.get('./account', listAccountController)
 
-server.get('./accounts', listAccountController)
+module.exports = router;
