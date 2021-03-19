9551ea3f2cf754588cd37113bcfb50d74fb1929c
Doreen Mensah <doreenradiance@gmail.com>
2021-03-19 12:26:05 +0000

    Created the field that needs to be validated

--- routes/bank.js
+++ routes/bank.js
@@ -1,23 +1,23 @@
 const router = require("express").Router();
-const {listBanksController,
+const { body } = require('express-validator')
+const { listBanksController,
     createBankController,
     updateBankController,
-    deleteBankController} = require("../controllers/bank")
+    deleteBankController } = require("../controllers/bank")
 
-        // // create account - post method
-router.post("/bank", 
-[body("name").notEmpty().withMessage("Account Name required"), 
-body("location").notEmpty().withMessage("Account Location required"),
-body("branch").notEmpty().withMessage("Account Branch required"),
-body("address").not().isEmpty().withMessage("Address required"),
-body("accountNumber").not().isEmpty().withMessage("AccountNumber required")],
-createAccountController)
+
 
 //routes
 // view bank - get method
 router.get("/bank/:id?", listBanksController)
 // // create bank - post method
-router.post("/bank", createBankController)
+router.post("/bank",
+    [body("name").trim().not().isEmpty().withMessage("Account Name required"),
+    body("location").notEmpty().withMessage("Account Location required"),
+    body("branch").notEmpty().withMessage("Account Branch required"),
+    body("address").not().isEmpty().withMessage("Address required"),
+    body("accountNumber").not().isEmpty().withMessage("AccountNumber required")],
+    createBankController)
 // update bank - put / patch method
 router.put("/bank", updateBankController)
 // // delete bank - delete method
