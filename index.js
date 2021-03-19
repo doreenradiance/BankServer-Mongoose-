ae93b3661984ffb5853023be2f049e98baf9ce20
Doreen Mensah <doreenradiance@gmail.com>
2021-03-19 11:34:22 +0000

    Cut and copied the bankRoutes and accountRoutes to their respective files

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-const { request } = require("express");
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
-const bankRouter = require("./routes");
-const accountRouter = require("./accountRouter")
-const {
-    listBanksController,
-    createBankController,
-    updateBankController,
-    deleteBankController
-} = require("./controllers")
 
 // create express server instance
 const server = express()
@@ -18,11 +8,7 @@ const server = express()
 //middlewares
 server.use(bodyParser.json());
 
-// define routes
-server.use(bankRouter)
 
-// define accountRouter
-server.use(accountRouter)
 
 // // connect database and start server
 mongoose.connect("mongodb+srv://codetrainUser:doreen1@cluster0.dlih7.mongodb.net/codetrain?retryWrites=true&w=majority",
